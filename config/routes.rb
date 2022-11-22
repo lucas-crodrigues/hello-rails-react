a527a67352165b6375ad3dd87b7319a5dedee34a
lucascr <lcrodrigues1995@gmail.com>
2022-11-22 16:18:00 -0300

    Generate Static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'static/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'static#index'
+  
   namespace :api do
     namespace :v1 do
       resources :greetings  
