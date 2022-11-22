3a4f22f1cf46fc64ddf694d658dff33c9907f3be
lucascr <lcrodrigues1995@gmail.com>
2022-11-22 15:10:26 -0300

    Generate controller Static

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'static#index'
+  
   namespace :api do
     namespace :v1 do
       resources :greetings  
